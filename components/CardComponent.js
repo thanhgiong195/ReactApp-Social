bd1a42d0b8cdba6b12006c9a9578d59022e2cf79
ThanhGiong <giongbt@gmail.com>
2019-03-08 15:57:00 +0700

    fix UI

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -3,6 +3,8 @@ import { Text, Image } from 'react-native'
 import {Card,CardItem, Thumbnail,Body,Left,Right,Button,Icon} from 'native-base'
 
 export default class CardComponent extends Component {
+
+  
   render() {
     return (
       <Card>
@@ -10,7 +12,7 @@ export default class CardComponent extends Component {
           <Left>
             <Thumbnail source={require('../images/avarta1.jpg')} />
             <Body>
-              <Text>BebeGalaxy</Text>
+              <Text>{this.props.userName}</Text>
               <Text note style={{fontSize:12}}>Jan 15, 2018</Text>
             </Body>
           </Left>
@@ -19,6 +21,23 @@ export default class CardComponent extends Component {
           <Image source={this.props.imageSource}
             style={{height: 200, width: null, flex: 1}}
           />
+        </CardItem>
+        <CardItem>
+          <Left>
+            <Button transparent>
+              <Icon active name="thumbs-up" />
+              <Text>12 Likes</Text>
+            </Button>
+          </Left>
+          <Body>
+            <Button transparent>
+              <Icon active name="chatbubbles" />
+              <Text>4 Comments</Text>
+            </Button>
+          </Body>
+          <Right>
+            <Text>11h ago</Text>
+          </Right>
         </CardItem>
       </Card>
     )
